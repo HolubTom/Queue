184f923807f89fb29b41aa3d462f449dd1941ded
Tomáš Holub <tomas.holub@webadvice.cz>
2015-01-31 00:45:47 +0100

    First draft of implementation of Message object

--- pCore/module/queue/messages/Message.py
+++ pCore/module/queue/messages/Message.py
@@ -1,10 +1,35 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+
 
 class Message(object):
 
+    STATUS_NEW = u"new"
+    STATUS_QUEUED = u"queued"
+    STATUS_DEQUEUED = u"dequeued"
+
+    def __init__(self):
+        self.__status = self.STATUS_NEW
+        self.__queuedDate = None
+        self.__dequeuedDate = None
+
     def setQueued(self):
-        pass
+        self.__status = self.STATUS_QUEUED
+        self.__queuedDate = datetime.now()
 
     def setDequeued(self):
-        pass
+        self.__status = self.STATUS_DEQUEUED
+        self.__dequeuedDate = datetime.now()
+
+    @property
+    def status(self):
+        return self.__status
+
+    @property
+    def queuedDate(self):
+        return self.__queuedDate
+
+    @property
+    def dequeuedDate(self):
+        return self.__dequeuedDate
---
+++ tests/messages/MessageTest.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from pCore.module.queue.messages.Message import Message
+from tests.BaseTest import BaseTest
+from datetime import datetime
+
+
+class MessageTest(BaseTest):
+
+    def setUp(self):
+        super(MessageTest, self).setUp()
+
+        self.instance = Message()
+
+    def testShouldMarkMessageAsQueued(self):
+        self.instance.setQueued()
+
+        self.assertEqual(Message.STATUS_QUEUED, self.instance.status)
+        self.assertIsInstance(self.instance.queuedDate, datetime)
+
+    def testShouldMarkMessageAsDequeued(self):
+        self.instance.setDequeued()
+
+        self.assertEqual(Message.STATUS_DEQUEUED, self.instance.status)
+        self.assertIsInstance(self.instance.dequeuedDate, datetime)
